47a246f96fe9382be8a63efa0934e4b7865dba94
sebastianlealj10 <sebastianlealj10@gmail.com>
2017-08-11 10:05:39 -0500

    final

--- app/src/main/java/sebasdeveloper/chromareaderv2/PreprocessingActivity.java
+++ app/src/main/java/sebasdeveloper/chromareaderv2/PreprocessingActivity.java
@@ -21,6 +21,7 @@ import org.opencv.core.Core;
 import org.opencv.core.CvType;
 import org.opencv.core.Mat;
 import org.opencv.imgcodecs.Imgcodecs;
+import org.opencv.imgproc.Imgproc;
 
 public class PreprocessingActivity extends AppCompatActivity {
     Intent intent;
@@ -30,7 +31,8 @@ public class PreprocessingActivity extends AppCompatActivity {
     Bitmap bmp;
     Mat ima;
     Mat imagray;
-    double[] dato={0,0,0};
+    double[] datocolor={0,0,0};
+    double[] datogray={0};
     //Clase donde se crea el layout y se inicializa la libreria ButterKnife
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,9 +40,10 @@ public class PreprocessingActivity extends AppCompatActivity {
         setContentView(R.layout.activity_preprocessing);
         ButterKnife.bind(this);
         ima=imread_mat();
-        deletebackground();
+        imagray=deletebackground();
        // ima=rotateima(ima);
-        imwrite_mat(ima);
+       // imwrite_mat(ima);
+        imwrite_mat(imagray);
         showima();
     }
     @OnClick(R.id.button8)
@@ -55,13 +58,15 @@ public class PreprocessingActivity extends AppCompatActivity {
         intent = new Intent(this, OrganicActivity.class);
         startActivity(intent);
     }
-    public void deletebackground(){
-       // Imgproc.cvtColor(ima, imagray, Imgproc.COLOR_RGB2GRAY);
+    public Mat deletebackground(){
+/*/
         int rows=ima.rows();
         int cols=ima.cols();
         int ch = ima.channels();
+
      //   double[] pix=ima.get(100,100);
       //  txt1.setText(Double.toString(pix[0])+" "+Double.toString(pix[1])+" "+Double.toString(pix[2]));
+
         {
             for (int i=0; i<rows; i++)
             {
@@ -70,12 +75,28 @@ public class PreprocessingActivity extends AppCompatActivity {
                     for (int k=0; k<ch; k++) {
                     double[] pix = ima.get(i, j);
                         if (pix[0] < 200 || pix[1] < 200 || pix[2] < 200 ) {
-                        ima.put(i, j, dato);}
+                        ima.put(i, j, datocolor);}
                     }
                  }
             }
         }
-   }
+
+/*/
+        Mat imaGray1 = new Mat();
+        Imgproc.cvtColor(ima, imaGray1, Imgproc.COLOR_BGR2GRAY);
+
+        int rows=imaGray1.rows();
+        int cols=imaGray1.cols();
+            for (int i=0; i<rows; i++) {
+                for (int j=0; j<cols; j++) {
+                    double[] pix = imaGray1.get(i, j);
+                    if (pix[0] < 200) {
+                        imaGray1.put(i, j, datogray);
+                    }
+                }
+            }
+        return imaGray1;
+    }
 
     public Mat rotateima(Mat imagen){
         Core.transpose(imagen,imagen);
