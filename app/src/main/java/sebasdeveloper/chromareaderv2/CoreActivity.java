e7c99387f7a18da3b10c08b481c4f9b3ef2b5ef5
Jefferson David Rojas Novoa <fejerson108@gmail.com>
2017-09-06 22:36:55 -0500

    prueba

--- app/src/main/java/sebasdeveloper/chromareaderv2/CoreActivity.java
+++ app/src/main/java/sebasdeveloper/chromareaderv2/CoreActivity.java
@@ -1,4 +1,5 @@
 package sebasdeveloper.chromareaderv2;
+
 import android.app.Activity;
 import android.content.Intent;
 import android.graphics.Bitmap;
@@ -33,7 +34,7 @@ public class CoreActivity extends AppCompatActivity {
     private static final String TAG = "MainActivity";
 
     static {
-        if(!OpenCVLoader.initDebug()){
+        if (!OpenCVLoader.initDebug()) {
             Log.d(TAG, "OpenCV not loaded");
         } else {
             Log.d(TAG, "OpenCV loaded");
@@ -50,8 +51,9 @@ public class CoreActivity extends AppCompatActivity {
     ImageView img;
     @BindView(R.id.button2)
     Button btn2;
-    final static int captureimage=0;
-    final static int loadimage=1;
+    final static int captureimage = 0;
+    final static int loadimage = 1;
+
     @Override
     //Clase donde se crea el layout y se inicializa la libreria ButterKnife
     protected void onCreate(Bundle savedInstanceState) {
@@ -59,12 +61,14 @@ public class CoreActivity extends AppCompatActivity {
         setContentView(R.layout.activity_core);
         ButterKnife.bind(this);
     }
+
     //Opcional
     @Override
-    public boolean onCreateOptionsMenu(Menu menu){
-        getMenuInflater().inflate(R.menu.main,menu);
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main, menu);
         return true;
     }
+
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
 
@@ -98,24 +102,21 @@ public class CoreActivity extends AppCompatActivity {
 
     //Clase donde se muestra la foto en el imageview provenga de la camara o de la galeria
     @Override
-    protected void onActivityResult(int requestCode,int resultCode, Intent data)
-    {
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
         //Condicion para la camara
-        if(resultCode == Activity.RESULT_OK  &&  requestCode == captureimage)
-        {
+        if (resultCode == Activity.RESULT_OK && requestCode == captureimage) {
             //funciones del opencv
-            ima =imread_mat();
+            ima = imread_mat();
             //no rotar si es en movil
-            ima=rotateima(ima);
+            ima = rotateima(ima);
             //
             imwrite_mat(ima);
             showima();
             btn2.setEnabled(true);
         }
         //Condicion para la galeria
-        if(resultCode == Activity.RESULT_OK  &&  requestCode == loadimage)
-        {
+        if (resultCode == Activity.RESULT_OK && requestCode == loadimage) {
             imageUri = data.getData();
             //se contruye la imagen en bitmap a partir del uri de la imagen
             try {
@@ -125,13 +126,12 @@ public class CoreActivity extends AppCompatActivity {
             }
             //se graba el bitmap en un directorio externo
             try {
-                FileOutputStream out = new FileOutputStream(Environment.getExternalStorageDirectory()+
-                        "/sebas/"+"cromaoriginal.jpg");
+                FileOutputStream out = new FileOutputStream(Environment.getExternalStorageDirectory() +
+                        "/sebas/" + "cromaoriginal.jpg");
                 bmp2.compress(Bitmap.CompressFormat.JPEG, 100, out);
                 out.flush();
                 out.close();
-            }
-            catch (Exception e) {
+            } catch (Exception e) {
                 e.printStackTrace();
             }
             img.setImageBitmap(bmp2);
@@ -139,31 +139,35 @@ public class CoreActivity extends AppCompatActivity {
 
         }
     }
+
     // Boton donde se lanza la nueva actividad
     @OnClick(R.id.button2)
-    public void processingphoto(View view)
-    {
+    public void processingphoto(View view) {
         intent = new Intent(this, PreprocessingActivity.class);
         startActivity(intent);
     }
-    public Mat imread_mat(){
+
+    public Mat imread_mat() {
         Mat imagen;
         //Se lee la foto desde la ubicacion donde fue almacenada en la memoria interna
-        imagen = Imgcodecs.imread(Environment.getExternalStorageDirectory()+
-                "/sebas/"+"foto1.jpg");
+        imagen = Imgcodecs.imread(Environment.getExternalStorageDirectory() +
+                "/sebas/" + "foto1.jpg");
         return imagen;
     }
-    public void imwrite_mat(Mat imagen){
-        Imgcodecs.imwrite(Environment.getExternalStorageDirectory()+
-                "/sebas/"+"foto1.jpg",imagen);
+
+    public void imwrite_mat(Mat imagen) {
+        Imgcodecs.imwrite(Environment.getExternalStorageDirectory() +
+                "/sebas/" + "foto1.jpg", imagen);
     }
-    public void showima(){
-        bmp = BitmapFactory.decodeFile(Environment.getExternalStorageDirectory()+
-                "/sebas/"+"foto1.jpg");
+
+    public void showima() {
+        bmp = BitmapFactory.decodeFile(Environment.getExternalStorageDirectory() +
+                "/sebas/" + "foto1.jpg");
         img.setImageBitmap(bmp);
     }
-    public Mat rotateima(Mat imagen){
-        Core.transpose(imagen,imagen);
+
+    public Mat rotateima(Mat imagen) {
+        Core.transpose(imagen, imagen);
         return imagen;
     }
 }
